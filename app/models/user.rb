401cc8903d4b8647150d835a39e23a2de7ff0623
i0n <ianalexanderwood@gmail.com>
2012-06-26 15:57:23 +0100

    removed email validation as the correct regex is overkill

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,6 @@ class User
 
   validates_presence_of :email, :message => "can't be blank"
   validates_uniqueness_of :email, :message => "Must be unique" 
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
-
   validates_presence_of :name, :message => "can't be blank"
 
 end
