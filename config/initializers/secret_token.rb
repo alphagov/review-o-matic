1a6bc186b46675d3700ef47cbeac8cc563285843
i0n <ianalexanderwood@gmail.com>
2012-06-26 10:19:35 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reviewomatic::Application.config.secret_token = '169eaa6e5a4869bfeebd95bb341aedf64280580eb9bb33d7755410f07ac8307c6e873f3a1ed0c98582bf6975d873b0263444eeeccf04f842ab3dc0920c69c28e'
