f83b059d3813a18505939257e252ee45bb5cf0f4
i0n <ianalexanderwood@gmail.com>
2012-06-26 15:20:16 +0100

    added very simple validation rules to models

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review
   field :user_id, :type => Integer
   field :mapping_id, :type => String
   field :result, :type => String
+  
+  validates_presence_of :user_id, :message => "can't be blank"
+  validates_presence_of :mapping_id, :message => "can't be blank"
+
 end
