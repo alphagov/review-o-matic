f4816d3eec84f41b274ce10ed433e340c738723d
Jordan Hatch <jordan@jordanh.net>
2012-06-26 16:55:02 +0000

    Added Devise to accept an access token to sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,24 @@
 class User
 
   include Mongoid::Document
+
+  devise :token_authenticatable, :rememberable, :trackable
+
   field :email, :type => String
   field :score, :type => Integer
   field :name, :type => String
 
+  field :authentication_token,  :type => String
+  field :current_sign_in_at,    :type => DateTime
+  field :last_sign_in_at,       :type => DateTime
+  field :current_sign_in_ip,    :type => String
+  field :last_sign_in_ip,       :type => String
+  field :sign_in_count,         :type => Integer
+  field :remember_created_at,   :type => DateTime
+
   validates_presence_of :email, :message => "can't be blank"
   validates_presence_of :name, :message => "can't be blank"
+
+  before_save :ensure_authentication_token
 
 end
