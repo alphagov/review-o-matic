6f52e80dbe9555d58b4d26470041350f4289ecb2
i0n <ianalexanderwood@gmail.com>
2012-06-27 10:17:04 +0100

    added calls to migratorator

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
+require 'migratorator_api'
 class HomeController < ApplicationController
   def index
-    
+    @users = User.all
+    @open_tags = ::MigratoratorApi::Mapping.all_by_tag('status:open') 
+    @closed_tags = ::MigratoratorApi::Mapping.all_by_tag('status:closed') 
   end
 end
