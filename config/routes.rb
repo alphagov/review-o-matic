524ea45bcf254e558b0c313bcda088742eee282b
Jordan Hatch <jordan@jordanh.net>
2012-06-27 09:26:33 +0000

    Remove sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Reviewomatic::Application.routes.draw do
   devise_for :users, :token_authentication_key => :access_token, :skip => [:sessions]
   devise_scope :user do
     get 'sign_in' => 'devise/sessions#new', :as => :new_user_session
-    post 'sign_in' => 'devise/sessions#create', :as => :user_session
     get 'sign_out' => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
