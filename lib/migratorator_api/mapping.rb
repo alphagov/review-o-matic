858993c75fba2a797394ab8023c5f82928d421ec
Jordan Hatch <jordan@jordanh.net>
2012-06-27 14:43:24 +0000

    Fixes api client when no query string, and returns OpenStructs for mappings

--- lib/migratorator_api/mapping.rb
+++ lib/migratorator_api/mapping.rb
@@ -2,21 +2,41 @@ class MigratoratorApi
   class Mapping
     def self.find_by_old_url(old_url, options = {})
       page = options[:page] || 1
-      format_mapping_reponse MigratoratorApi.client.get("/mappings.json?old_url=#{old_url}&page=#{page}")
+      parse_multiple_mappings_from_response MigratoratorApi.client.get("/mappings.json?old_url=#{old_url}&page=#{page}")
     end
 
     def self.find_by_new_url(new_url, options = {})
       page = options[:page] || 1
-      format_mapping_reponse MigratoratorApi.client.get("/mappings.json?new_url=#{new_url}&page=#{page}")
+      parse_multiple_mappings_from_response MigratoratorApi.client.get("/mappings.json?new_url=#{new_url}&page=#{page}")
+    end
+
+    def self.find_random_mapping(tags = '')
+      response = MigratoratorApi.client.get("/mappings/random.json?tags=#{tags}")
+      return false if response.code != "200"
+      OpenStruct.new( JSON.parse(response.body) )
+    end
+
+    def self.find_by_id(id)
+      parse_mapping_from_response MigratoratorApi.client.get("/mappings/#{id}.json")
     end
 
     def self.all_by_tag(tags, options = {})
       page = options[:page] || 1
-      format_mapping_reponse MigratoratorApi.client.get("/mappings.json?tags=#{tags}&page=#{page}")
+      parse_multiple_mappings_from_response MigratoratorApi.client.get("/mappings.json?tags=#{tags}&page=#{page}")
     end
 
-    def self.format_mapping_reponse(response)
-      mappings = JSON.parse(response.body)["mappings"] || []
+    def self.parse_multiple_mappings_from_response(response)
+      return false if response.code != "200"
+      (JSON.parse(response.body)["mappings"] || []).map {|m| format_mapping(m) }
+    end
+
+    def self.parse_mapping_from_response(response)
+      return false if response.code != "200"
+      format_mapping JSON.parse(response.body)
+    end
+
+    def self.format_mapping(hash)
+      OpenStruct.new( hash["mapping"] )
     end
   end
 end
