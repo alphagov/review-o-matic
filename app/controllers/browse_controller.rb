403785faf12308dc106448e522854ae039c507a2
Jordan Hatch <jordan@jordanh.net>
2012-06-28 12:13:18 +0000

    Browser continuously loads random mappings

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -2,13 +2,22 @@ class BrowseController < ApplicationController
 
   layout "browser"
 
+  respond_to :html, :json
+
   def index
     @mapping = MigratoratorApi::Mapping.find_random_mapping("status:closed")
-    redirect_to browse_mapping_path(@mapping._id)
+    puts @mapping.inspect
+    puts @mapping.id
+
+    respond_to do |format|
+      format.html { redirect_to browse_mapping_path(@mapping.id) }
+      format.json { render :json => { :next_mapping_id => @mapping.id } }
+    end
   end
 
   def show
     @mapping = MigratoratorApi::Mapping.find_by_id(params[:id])
+    @review = Review.where(:mapping_id => @mapping.id, :user_id => current_user.id).first
   end
 
 end
---
+++ app/helpers/browser_helper.rb
@@ -0,0 +1,9 @@
+module BrowserHelper
+  def button_attributes_for(review, result)
+    atts = { :name => result }
+    if review.present? and review.result == result
+      atts.merge!({ :class => "selected" })
+    end
+    atts
+  end
+end
