a9992575b9720752a2d4c3eda97c226f38ede5fa
i0n <ianalexanderwood@gmail.com>
2012-06-28 17:09:59 +0100

    updated view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,12 +6,13 @@ class DashboardController < ApplicationController
 
     respond_to do |format|
       format.html do |x|
-        @open_tags = ::MigratoratorApi::Mapping.all_by_tag('status:open') 
-        @closed_tags = ::MigratoratorApi::Mapping.all_by_tag('status:closed') 
-        @all_tags_count = @open_tags.count + @closed_tags.count
+
       end
       format.js do |x|
         @mappings = Mapping.all
+        @green_mappings = @mappings.where(:score.gt => "80")
+        @amber_mappings = @mappings.where(:score.lt => "80", :score.gt => "20")
+        @red_mappings = @mappings.where(:score.lt => "20")
       end
     end
 
