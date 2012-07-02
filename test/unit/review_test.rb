6a2bb4c4c5ebdc6996936a1f44ce369894eb49e2
i0n <ianalexanderwood@gmail.com>
2012-07-02 12:24:38 +0100

    added basic model tests

--- test/unit/review_test.rb
+++ test/unit/review_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user_1 = User.find_or_create_by(:name => "Ian", :email => "ian.wood@digital.cabinet-office.gov.uk", :authentication_token => 'password')
+  end
+
+  teardown do
+    @user_1.delete
+  end
+
+  should_not allow_value(nil).for(:mapping_id)
+  should_not allow_value(nil).for(:user_id)
+  should_not allow_value(nil).for(:result)
+
+  should allow_value('positive').for(:result)
+  should allow_value('neutral').for(:result)
+  should allow_value('negative').for(:result)
+
+  should "Set a score for the user" do
+    @review_1 = Review.create(:user_id => @user_1.id, :mapping_id => "testo", :result => "positive") 
+    @review_1.set_user_score
+    assert_equal 1, @review_1.user.score
+  end
+
 end
