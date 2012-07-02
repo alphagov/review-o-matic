60050b0900beb9e84d980a120a880654df474f35
i0n <ianalexanderwood@gmail.com>
2012-07-02 17:50:41 +0100

    created index view for reviews so that editors can look at the worst rated mappings in the system

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,10 @@ class ReviewsController < ApplicationController
 
   respond_to :json
 
+  def index
+    @mappings = Mapping.asc(:score).limit(40)
+  end
+
   def update
     @review = Review.find_or_create_by(mapping_id: params[:id], user_id: current_user.id)
     @review.result = params[:result]
