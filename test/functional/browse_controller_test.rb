1e6d92f70677754e1143f44319d9cccf82d499a7
i0n <ianalexanderwood@gmail.com>
2012-07-02 14:09:31 +0100

    setting up stub in tests

---
+++ test/functional/browse_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class BrowseControllerTest < ActionController::TestCase
+
+  setup do
+    login_as_stub_user
+  end
+
+  #should "get the show page of the browse controller" do
+  #  get(:show, {'id' => "testo"})
+  #  assert_response :success
+  #end
+
+end
