6a48e3e70136df8a362c22e98bcf460f14cefb5f
Jordan Hatch <jordan@jordanh.net>
2012-07-04 13:48:32 +0000

    Simplify code for creating mappings

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review
   include Mongoid::Document
-  belongs_to :mapping
+  belongs_to :mapping, :foreign_key => "mapping_id"
   belongs_to :user
+
   field :user_id, :type => String
   field :mapping_id, :type => String
   field :result, :type => String
@@ -15,13 +16,11 @@ class Review
   validates :user_id, :mapping_id, :result, :presence => true
   validates :result, :inclusion => { :in => RESULTS }
 
+  before_create :ensure_mapping_exists, :if => proc { self.mapping.blank? }
+
   after_save :set_user_score
   after_save :set_mapping_score
   after_save :set_mapping_reviews_count
-
-  def mapping
-    @mapping ||= Mapping.find_or_create_by(:mapping_id => mapping_id)
-  end
 
   def set_user_score
     self.user.set_score!
@@ -33,6 +32,10 @@ class Review
 
   def set_mapping_reviews_count
     mapping.set_reviews_count!
+  end
+
+  def ensure_mapping_exists
+    self.mapping = Mapping.find_or_create_by(:mapping_id => self.mapping_id)
   end
 
 end
