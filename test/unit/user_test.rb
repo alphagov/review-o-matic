acfc71c7ecd224c7aeee8f8c5b9de5cb825c3e16
i0n <ianalexanderwood@gmail.com>
2012-07-11 17:57:46 +0100

    changed review model ready for comments and no scoring and added method to user model to check if the user is a member of gds

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user_1 = User.find_or_create_by(:name => "Ian", :email => "ian.wood@digital.cabinet-office.gov.uk", :authentication_token => 'password')
+  end
+
+  should "return true when running gds_user? if the users email is a gds email" do
+    assert_equal true, @user_1.gds_user?
+  end
+
+  should "return false when running gds_user? if the users email is not a gds email" do
+    @user_1.email = "ian@notgds.com"
+    assert_equal false, @user_1.gds_user?
+  end
+
 end
