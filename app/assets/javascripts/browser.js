48c71cef5499ca5dfd3b64ad1c6fa0391e0f25d8
i0n <ianalexanderwood@gmail.com>
2012-07-12 18:16:23 +0100

    working on node browser

--- app/assets/javascripts/browser.js
+++ app/assets/javascripts/browser.js
@@ -140,3 +140,59 @@ $(document).ready( function() {
     });
   }
 });
+
+
+
+(function ($) {
+var NULL_MAPPING_URL = 'http://4.bp.blogspot.com/_CBk-5X4QQvQ/TH-kNuQ8GpI/AAAAAAAAB7U/BzXdt6teQe8/s1600/Cat-fixes-your-computer.jpg';
+var NO_MAPPINGS_URL = 'http://4.bp.blogspot.com/_CBk-5X4QQvQ/TH-kNuQ8GpI/AAAAAAAAB7U/BzXdt6teQe8/s1600/Cat-fixes-your-computer.jpg';
+
+var curUrl;
+var offDomainWarning = "You've now navigated off the chosen domain. No updates will occur to the right-hand panel.";
+var shownOffDomainWarning = false;
+
+function updateFrame(url) {
+  $.getJSON('/__api/api/mappings.json', {old_url: url})
+    .then(function (data) {
+      var m;
+      if (typeof data.mappings !== 'undefined' && data.mappings.length > 0) {
+        m = data.mappings[0].mapping.new_url;
+      } else {
+        m = NO_MAPPINGS_URL;
+      }
+      if (m == null) {
+        m = NULL_MAPPING_URL;
+      }
+      $('#new').attr('src', m);
+    });
+}
+
+function updateUrl() {
+  url = $('#old')[0].contentWindow.location.pathname;
+  
+  if (url === 'blank') { return; }
+  if (url === curUrl) { return; }
+  curUrl = url;
+
+  if (typeof url === 'undefined') {
+    if (shownOffDomainWarning) { return; }
+
+    if (confirm(offDomainWarning)) {
+      location.href = '/__';
+    } else {
+      shownOffDomainWarning = true;
+    }
+  } else {
+    updateFrame(url);
+    console.log(url);
+    history.pushState({}, "", '/__' + url);      
+  }
+}
+
+function init() {
+  $('#old').attr('src', window.location.pathname.replace(/^\/__/, "") || '/');
+  updateTimer = setInterval(updateUrl, 500);
+}
+
+$(init);
+}(jQuery));
