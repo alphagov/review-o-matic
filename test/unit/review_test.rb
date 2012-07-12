1cc7de4641b0558d7e1f32d61da0ea8d18ffae96
i0n <ianalexanderwood@gmail.com>
2012-07-12 10:50:02 +0100

    after getting feedback on reviewomatic we need to keep positive and negative buttons so added validation back into model and changed test

--- test/unit/review_test.rb
+++ test/unit/review_test.rb
@@ -9,9 +9,9 @@ class ReviewTest < ActiveSupport::TestCase
 
   should_not allow_value(nil).for(:mapping_id)
   should_not allow_value(nil).for(:user_id)
+  should_not allow_value(nil).for(:result)
 
   should allow_value('positive').for(:result)
-  should allow_value('neutral').for(:result)
   should allow_value('negative').for(:result)
 
   should "Set a score for the user" do
