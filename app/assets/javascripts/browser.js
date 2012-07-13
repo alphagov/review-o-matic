cf22e9fbdfb893d6707c7356fbd18559668d6f01
i0n <ianalexanderwood@gmail.com>
2012-07-13 09:57:07 +0100

    working on getting node browser integrated

--- app/assets/javascripts/browser.js
+++ app/assets/javascripts/browser.js
@@ -96,49 +96,49 @@ $(document).ready( function() {
     });
   }
 
-  function load_mapping(id) {
-    $('#overlay').fadeTo('fast', 0.9);
+  //function load_mapping(id) {
+  //  $('#overlay').fadeTo('fast', 0.9);
 
-    $.getJSON('/browse/'+ id +'.json', function(data) {
-      switch (data.status) {
-        case 301:
-          mapping_result = "→ <a href='" + data.new_url + "' target='blank'>" + data.new_url + "</a>";
-          new_page_src = data.new_url;
-          break;
-        case 410:
-          mapping_result = "→ Gone";
-          new_page_src = '/browser/410.html';
-          break;
-        default:
-          mapping_result = "None";
-          new_page_src = '/browser/no_status.html';
-      }
+  //  $.getJSON('/browse/'+ id +'.json', function(data) {
+  //    switch (data.status) {
+  //      case 301:
+  //        mapping_result = "→ <a href='" + data.new_url + "' target='blank'>" + data.new_url + "</a>";
+  //        new_page_src = data.new_url;
+  //        break;
+  //      case 410:
+  //        mapping_result = "→ Gone";
+  //        new_page_src = '/browser/410.html';
+  //        break;
+  //      default:
+  //        mapping_result = "None";
+  //        new_page_src = '/browser/no_status.html';
+  //    }
 
-      if (data.result !== null) {
-        $('.buttons button[name="'+ data.result +'"]').addClass('selected');
-      }
+  //    if (data.result !== null) {
+  //      $('.buttons button[name="'+ data.result +'"]').addClass('selected');
+  //    }
 
-      $('#mapping_title a').text(data.title).attr('href', data.old_url);
-      $('#mapping_result').html(mapping_result);
-      $('#mapping_old_page').attr('src', data.old_url);
-      $('.mapping_links_container .left').empty().append('<a href="' + data.old_url +'">' + data.old_url + '</a>');
-      $('#mapping_new_page').attr('src', new_page_src);
-      $('.mapping_links_container .right').empty().append('<a href="' + new_page_src +'">' + new_page_src + '</a>');
+  //    $('#mapping_title a').text(data.title).attr('href', data.old_url);
+  //    $('#mapping_result').html(mapping_result);
+  //    $('#mapping_old_page').attr('src', data.old_url);
+  //    $('.mapping_links_container .left').empty().append('<a href="' + data.old_url +'">' + data.old_url + '</a>');
+  //    $('#mapping_new_page').attr('src', new_page_src);
+  //    $('.mapping_links_container .right').empty().append('<a href="' + new_page_src +'">' + new_page_src + '</a>');
 
-      history.pushState( { mapping_id: data.id }, '', '/browse/' + data.id );
+  //    history.pushState( { mapping_id: data.id }, '', '/browse/' + data.id );
 
-      $('.user_score').text( parseInt( $('.user_score').text() ) + 1 );
+  //    $('.user_score').text( parseInt( $('.user_score').text() ) + 1 );
 
-      can_load_new_mapping = true;
-      $('.buttons form').attr('action', '/reviews/' + data.id);
-      $('.buttons button').removeAttr('disabled');
-    });
+  //    can_load_new_mapping = true;
+  //    $('.buttons form').attr('action', '/reviews/' + data.id);
+  //    $('.buttons button').removeAttr('disabled');
+  //  });
 
-    $('#mapping_old_page').load( function(){
-      $('.buttons').removeClass('disabled');
-      $('#overlay').fadeTo('fast', 0);
-    });
-  }
+  //  $('#mapping_old_page').load( function(){
+  //    $('.buttons').removeClass('disabled');
+  //    $('#overlay').fadeTo('fast', 0);
+  //  });
+  //}
 });
 
 
