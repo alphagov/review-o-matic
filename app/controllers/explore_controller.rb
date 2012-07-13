3cfad6e8c9e36b740868d6cc6b4671015b819604
i0n <ianalexanderwood@gmail.com>
2012-07-13 16:03:15 +0100

    working on node browser

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,7 +1,11 @@
 class ExploreController < ApplicationController
 
-  def index
-    
+  respond_to :html, :json
+
+  def show
+    @old_url = params[:old_url]
+    @mapping = MigratoratorApi::Mapping.find_by_old_url(@old_url).first
+    #@review = Review.where(:mapping_id => @mapping.id, :user_id => current_user.id).first
   end
 
 end
