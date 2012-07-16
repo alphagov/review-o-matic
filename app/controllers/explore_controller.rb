ee1a8ad9057660aed6321ca0702aab42b7b37f0c
i0n <ianalexanderwood@gmail.com>
2012-07-16 17:11:59 +0100

    added path to explore app using Plek

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -5,7 +5,6 @@ class ExploreController < ApplicationController
   def show
     @old_url = params[:old_url]
     @mapping = MigratoratorApi::Mapping.find_by_old_url(@old_url).first
-    #@review = Review.where(:mapping_id => @mapping.id, :user_id => current_user.id).first
   end
 
 end
