c6d8e7f3648acd092fb53db1712ba3f9886308e9
i0n <ianalexanderwood@gmail.com>
2012-07-17 15:34:31 +0100

    changed asset path for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,8 @@ Reviewomatic::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( browser.js colorJizz.js dashboard_poll.js mosaic_poll.js )
 
+  config.assets.prefix = "/__/assets"
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
