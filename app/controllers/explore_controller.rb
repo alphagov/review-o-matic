34ec7cd20ff03cbbbae4c56f6da4e589fb6a6d74
i0n <ianalexanderwood@gmail.com>
2012-07-18 12:37:30 +0100

    changed migratorator api for old mappings and handled a mapping not being found in explore controller

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -2,9 +2,23 @@ class ExploreController < ApplicationController
 
   respond_to :html, :json
 
+  rescue_from NoMethodError, :with => :mapping_not_found
+
   def show
-    @old_url = params[:old_url]
-    @mapping = MigratoratorApi::Mapping.find_by_old_url(@old_url).first
+    @mapping = MigratoratorApi::Mapping.find_by_old_url( params[:old_url] )
+  end
+
+  private
+
+  def mapping_not_found
+    respond_to do |format|
+      format.html do |x|
+        render 'public/404.html', :status => 404
+      end
+      format.json do |x|
+        render :status => 404, :json => { :status => 404, :message => 'Mapping not found.' }
+      end
+    end
   end
 
 end
