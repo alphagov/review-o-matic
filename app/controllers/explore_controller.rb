f647668be2615c6c4cd4f9c662c34f91a20020ed
i0n <ianalexanderwood@gmail.com>
2012-07-19 11:08:35 +0100

    added logging to debug preview

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -6,6 +6,8 @@ class ExploreController < ApplicationController
 
   def show
     @mapping = MigratoratorApi::Mapping.find_by_old_url( params[:old_url] )
+    Rails.logger.debug "@mapping instance variable in Explore controller"
+    Rails.logger.debug @mapping
   end
 
   private
