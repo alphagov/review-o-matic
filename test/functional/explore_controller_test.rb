c39d993a1223172f354c8ca7c24121194cd7e72d
i0n <ianalexanderwood@gmail.com>
2012-07-20 14:13:20 +0100

    fixed incorrect handling of mappings in explore view

--- test/functional/explore_controller_test.rb
+++ test/functional/explore_controller_test.rb
@@ -3,13 +3,20 @@ require 'test_helper'
 class ExploreControllerTest < ActionController::TestCase
 
   setup do
-
+    login_as_gds_stub_user
   end
 
   should "get the show page of the explore controller" do
-
+    MigratoratorApi::Mapping.stubs(:find_by_old_url).returns(MockMigratorator.new)
+    get :show
+    assert_response :success
   end
 
-
+  should "get the show page and return JSON if record is not found and format is JSON" do
+    MigratoratorApi::Mapping.stubs(:find_by_old_url).returns(false)
+    get :show, format: 'json'
+    assert_response :success
+    assert_equal JSON.generate({ :status => 404, :message => 'Mapping not found.' }), @response.body
+  end
 
 end
