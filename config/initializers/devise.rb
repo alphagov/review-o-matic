44e0dfa885aae2a6a16c8221513fd1d788eff8fa
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-07-26 07:11:23 -0700

    Changing wording, subject line and email address from of reset password email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "Winston Smith-Churchill <winston@alphagov.co.uk>"
+  config.mailer_sender = "GDS Transition Team <govuk-transition@digital.cabinet-office.gov.uk>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
