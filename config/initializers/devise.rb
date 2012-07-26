7452c5d2f48f942dc6a09405ae2787754aa7ce44
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-07-26 07:55:59 -0700

    changing email sender back to Winston

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "GDS Transition Team <govuk-transition@digital.cabinet-office.gov.uk>"
+  config.mailer_sender = "GDS Transition Team <winston@alphagov.co.uk>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
