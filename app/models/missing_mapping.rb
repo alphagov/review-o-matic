1efa8a108c5a8389dd4eabac92322e277b5713ab
i0n <ianalexanderwood@gmail.com>
2012-08-01 18:14:49 +0100

    fixed custom directgov mapping validation in MissingMapping

--- app/models/missing_mapping.rb
+++ app/models/missing_mapping.rb
@@ -6,12 +6,12 @@ class MissingMapping
 
   validates_presence_of :old_url
   validates_uniqueness_of :old_url
-  validate :direct_gov_url_validation
+  validate :direct_gov_url_validation, :if => proc { !self.old_url.nil? }
 
   def direct_gov_url_validation
     advanced_search_starting_string = 'http://www.direct.gov.uk/en/AdvancedSearch/'
     advanced_search_results = '/results'
-    if self.old_url.match(/^#{advanced_search_starting_string}{1}?#{advanced_search_results}{1}$/)
+    if old_url.match(/^#{advanced_search_starting_string}{1}.*#{advanced_search_results}{1}$/)
       errors.add(:old_url, 'This url is the results of a Directgov Advanced Search so does not require a mapping on GOV.UK')
     end
   end
