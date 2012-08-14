acc8f594f2448fb7dbd471c5285d365648ba2f96
i0n <ianalexanderwood@gmail.com>
2012-08-14 17:24:53 +0100

    added csv output for reviews index route

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,35 @@
+require 'csv'
+
 class ReviewsController < ApplicationController
 
   respond_to :json
 
   def index
-    @mappings = Mapping.where(:reviews_count.gt => 0).order_by(:reviews_count.desc).page params[:page]
-    mapping_ids = []
-    @mappings.each {|mapping| mapping_ids << mapping.mapping_id.to_s }
-    @migratorator_mappings = MigratoratorApi::Mapping.find_by_ids(mapping_ids.join(','))
+    respond_to do |format|
+      format.html do
+        mapping_ids = []
+        @mappings = Mapping.where(:reviews_count.gt => 0).order_by(:reviews_count.desc).page params[:page]
+        @mappings.each {|mapping| mapping_ids << mapping.mapping_id.to_s }
+        @migratorator_mappings = MigratoratorApi::Mapping.find_by_ids(mapping_ids.join(','))
+      end
+      format.csv do 
+        mapping_ids = []
+        @mappings = Mapping.where(:reviews_count.gt => 0).order_by(:reviews_count.desc)
+        @reviews = Review.order_by(:mapping_id.desc)
+        @mappings.each {|mapping| mapping_ids << mapping.mapping_id.to_s }
+        @migratorator_mappings = MigratoratorApi::Mapping.find_by_ids(mapping_ids.join(','))
+        csv = CSV.generate do |csv|
+          csv << ["User", "Old Url", "New Url", "Comment"]
+          @reviews.each do |review|
+            if !@migratorator_mappings[review.mapping.mapping_id].nil?
+              csv << [review.user.email, @migratorator_mappings[review.mapping.mapping_id]['old_url'], @migratorator_mappings[review.mapping.mapping_id]['new_url'], review.comment]
+            end
+          end
+        end
+        send_data(csv, :type => 'test/csv', :filename => 'Mapping_Reviews.csv') 
+      end
+    end
+
   end
 
   # Custom create route which requires an :id parameter for explorer view
