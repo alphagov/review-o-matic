405118462da9997e130b41b9576f422c26f1a858
i0n <ianalexanderwood@gmail.com>
2012-08-15 12:54:52 +0100

    refectored reviews index csv route to work with migratorator api limit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,19 +5,29 @@ class ReviewsController < ApplicationController
   respond_to :json
 
   def index
+    mapping_ids = []
     respond_to do |format|
       format.html do
-        mapping_ids = []
         @mappings = Mapping.where(:reviews_count.gt => 0).order_by(:reviews_count.desc).page params[:page]
         @mappings.each {|mapping| mapping_ids << mapping.mapping_id.to_s }
         @migratorator_mappings = MigratoratorApi::Mapping.find_by_ids(mapping_ids.join(','))
       end
       format.csv do 
-        mapping_ids = []
+        mapping_ids_collections = []
         @mappings = Mapping.where(:reviews_count.gt => 0).order_by(:reviews_count.desc)
-        @reviews = Review.order_by(:mapping_id.desc)
         @mappings.each {|mapping| mapping_ids << mapping.mapping_id.to_s }
-        @migratorator_mappings = MigratoratorApi::Mapping.find_by_ids(mapping_ids.join(','))
+
+        # MigratoratorApi::Mapping.find_by_ids(collection.join(',')) has to be limited to 50 ids as there is a limit to the size of the query string. This makes multiple API calls and then merges the results. Could be refactored.
+        ((mapping_ids.count / 50) + 1).times do
+          mapping_ids_collections << mapping_ids.shift(50)
+        end
+        @migratorator_mappings = {} 
+        mapping_ids_collections.each do |collection|
+          @migratorator_mappings.merge!(MigratoratorApi::Mapping.find_by_ids(collection.join(',')))
+        end
+
+        @reviews = Review.order_by(:mapping_id.desc)
+        
         csv = CSV.generate do |csv|
           csv << ["User", "Old Url", "New Url", "Comment"]
           @reviews.each do |review|
